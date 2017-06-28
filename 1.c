e441e6108ebeb2b6b1a7eb8f7b91f988f89e717b
root <dineshece1035@gmail.com>
2017-06-28 09:58:12 +0530

    dinesh

---
+++ 1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<signal.h>
+#include<string.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+main()
+{
+	int m;
+        int d=999;
+	char aa='Z';
+	m=d-(int)aa;
+	printf("Dinesh");
+	printf("Dinesh");
+	printf("\rchaudhary\n");
+        /*fd = open(argv[1],O_RDONLY);
+        if (fd < 0)
+        {
+                printf("File opening failed \n");
+                exit(1);
+        }
+        while (1)
+        {
+                retr=read(fd, buf, 1);
+                retw=write(1, buf, retr);
+        }*/
+}
